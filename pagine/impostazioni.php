e6bfa3ce22da1543d4854d5d41934e332185bfab
Massimo Saiani <massimo.saiani@buonarroti.tn.it>
2024-08-31 16:59:34 +0200

    testing ora solare e legale

--- pagine/impostazioni.php
+++ pagine/impostazioni.php
@@ -6,37 +6,54 @@ $con = accesso();
     /*
     Per modificare il testo visualizzato sulla homepage.
     */
-    $_SESSION["anno_scolastico"]= $__settings->config->anno_scolastico;
+    $_SESSION["anno_scolastico"]= $__settings->config->annoScolastico;
     $_SESSION["permessi_ora_inizio_stringa"]= $__settings->config->permessiOraInizio;
     $_SESSION["permessi_ora_fine_stringa"]= $__settings->config->permessiOraFine;
     $_SESSION["limita_orario_permessi"]= $__settings->config->limitaOrarioPermessi;
  
     // Create a DateTime object for a specific date and time
     $dateTime = new DateTime("");
-    echo $dateTime->format('Y-m-d H:i:s');
+    echo "ora attuale " . $dateTime->format('Y-m-d H:i:s');
 
     // Check Daylight Saving Time
     $isDST = $dateTime->format("I");
-    echo $isDST;
+//    echo "dst " . $isDST;
 
-    // 1 ora legale (Estate)
-    // 0 ora solare (Inverno)
+    // 0 ora legale (Estate)
+    // 1 ora solare (Inverno)
 
-    if ($isDST == 1)
+    $inizio = new DateTime($__settings->config->permessiOraInizio);
+//    echo $inizio->format('Y-m-d H:i:s');
+    $fine = new DateTime($__settings->config->permessiOraFine);
+//    echo $fine->format('Y-m-d H:i:s');
+
+    if ($isDST == 0)
     {
-      $inizio = new DateTime($__settings->config->permessiOraInizio);
-      echo $inizio->format('Y-m-d H:i:s');
-      $fine = new DateTime($__settings->config->permessiOraFine);
-      echo $fine->format('Y-m-d H:i:s');
-      $inizio->add(new DateInterval('PT1H'));
-      echo $inizio->format('Y-m-d H:i:s');
-      $fine->add(new DateInterval('PT1H'));
-      echo $fine->format('Y-m-d H:i:s');
+      $inizio->sub (new DateInterval('PT2H'));
+//      echo $inizio->format('Y-m-d H:i:s');
+      $fine->sub(new DateInterval('PT2H'));
+//      echo $fine->format('Y-m-d H:i:s');  
+    }
+    else
+    {
+      $inizio->sub(new DateInterval('PT1H'));
+//      echo $inizio->format('Y-m-d H:i:s');
+      $fine->sub(new DateInterval('PT1H'));
+//      echo $fine->format('Y-m-d H:i:s');  
+    }
   
-      $_SESSION["permessi_ora_inizio"] = $inizio->format("H:i");
-      $_SESSION["permessi_ora_fine"] = $fine->format("H:i");
-      echo $inizio->format("H:i");
-      echo $fine->format("H:i");
+    $_SESSION["permessi_ora_inizio"] = $inizio->format("H:i");
+    $_SESSION["permessi_ora_fine"] = $fine->format("H:i");
+//    echo $inizio->format("H:i");
+//    echo $fine->format("H:i");
+    $ora= date("H:i");
+    if ( $ora<$_SESSION["permessi_ora_fine"] || $ora>$_SESSION["permessi_ora_inizio"] )
+    {
+        echo "permessi abilitati.";
+    }
+    else
+    {
+        echo "permessi non abilitati.";
     }
 
 ?>
