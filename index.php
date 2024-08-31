f4459572eb6bc3bda2fbff1c2f8d2cb5943fcdc9
Massimo Saiani <massimo.saiani@buonarroti.tn.it>
2024-08-31 13:41:21 +0200

    Ripulite cartelle essenziali

--- index.php
+++ index.php
@@ -22,25 +22,41 @@
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">-->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <style>
-body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif}
-.w3-bar,h1,button {font-family: "Montserrat", sans-serif}
-.fa-anchor,.fa-coffee {font-size:200px}
+  body,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    font-family: "Lato", sans-serif
+  }
+
+  .w3-bar,
+  h1,
+  button {
+    font-family: "Montserrat", sans-serif
+  }
+
+  .fa-anchor,
+  .fa-coffee {
+    font-size: 200px
+  }
 </style>
 
 
 
-<body>  
-    
-<?php             
-session_start();
-if(isset($_SESSION["loggato"]) && $_SESSION["loggato"]=="si"){
-  header("Location:pagine/frontoffice.php");
-//  header("Location:pagine/indexLogout.php");
-}
-else{
+<body>
+
+  <?php
+  session_start();
+  if (isset($_SESSION["loggato"]) && $_SESSION["loggato"] == "si") {
+    header("Location:pagine/frontoffice.php");
+  } else {
     header("Location: pagine/indexLogin.php");
-}
-?>
-  
+  }
+  ?>
+
 </body>
+
 </html>
