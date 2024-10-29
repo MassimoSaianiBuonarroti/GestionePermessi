d7b3789827266874593a7ccf9714c93a98b9d78c
Massimo Saiani <massimo.saiani@buonarroti.tn.it>
2024-10-29 10:47:51 +0100

    minor bugs

--- pagine/nuovoPermesso.php
+++ pagine/nuovoPermesso.php
@@ -23,14 +23,17 @@ global $settings;
 require_once('browser.php'); 
 $browser = new Browser();
 
+require_once 'load_settings.php';
+require_once 'impostazioni.php';
+require_once '../version.php';
+
 if(!isset($_SESSION["loggato"])){
     header("Location:../index.php");
 }
 date_default_timezone_set("Europe/Rome");
 $ora= date("H:i:s");
 
-if (!($ora<$_SESSION["permessi_ora_fine"] || $ora>$_SESSION["permessi_ora_inizio"])){
-    //echo "<br><br><button type='button' class='btn btn-danger' onclick=window.location.href='nuovoPermesso.php'>NUOVO PERMESSO</button>";
+if (!($ora<$__settings->config->permessiOraFine) || ($ora>$__settings->config->permessiOraInizio)){
     header("Location:../index.php");   
 }
 if($browser->getBrowser() == Browser::BROWSER_IE || $browser->getBrowser() == Browser::BROWSER_SAFARI){
@@ -84,39 +87,25 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif}
     <?php
     require_once 'accessoDatabase.php';
     $con= accesso();
-    //$query= "SELECT * FROM login WHERE idUtente=".$_SESSION["idutente"];
     $passw= md5($_SESSION["password"]);
     $query= "SELECT * FROM login WHERE nomeutente=".$_SESSION["nomeutente"];
-    //echo $query;
     
     $result=mysqli_query($con,$query);
     //echo mysqli_num_rows($result);
     if(mysqli_num_rows($result)>0){
         $row= mysqli_fetch_array($result);
-        $nomegenitore= $row["cognome_genitore"]." ".$row["nome_genitore"];
-        //$nomestudente= $row["cognome"]." ".$row["nome"];
-        //$classe= $row["classe"];*/       
+        $nomegenitore= $row["cognome_genitore"]." ".$row["nome_genitore"];   
     }
     ?>
     <form name="nuovoPermesso" action="#bottom" method="POST">    
         <div>
-            <label>
-                <!--<input type="radio" class="option-input radio" name='opzione' value="entrata" checked />-->
-                <!--ENTRATA-->
-            </label>
             <label>
               <input type="radio" class="option-input radio" name='opzione' value="uscita" checked/>
              USCITA
             </label>
             
         </div>
-        
-        
-        
-        <!--
-        <input type='radio' name='opzione' value="entrata" checked> ENTRATA<br><br>
-        <input type='radio' name='opzione' value="uscita" > USCITA<br><br>-->
-        <!--<input type='radio' name='opzione' value="assenza" > ASSENZA<br><br>-->
+
         <div style="padding-top:2%">DATA <input type="date" name="data" placeholder="Data" required></div>
         <div></div><br>
         <!--<p><input type="text" name="orauscita" placeholder="Ora HH:MM"></p>-->      
@@ -136,65 +125,47 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif}
             }            
             echo "</select>";
         }
-        ?>
-        <!--<p><input type="text" name="cognomenomestudente" placeholder="Cognome e Nome dello studente" value="<?php //echo $nomestudente?>"></p>
-        <p><input type="text" name="classe" placeholder="Classe" value="<?php //echo $classe?>"></p>-->
-        <!--<p><input type="text" name="motivazione" placeholder="Motivazione"></p> -->       
+        ?> 
         <br><br>
         <p>MOTIVAZIONE RICHIESTA PERMESSO<br><textarea name="note" rows="7" cols="40" required></textarea></p>
         <p><input type="hidden" name="tipo" value="com"></p>
         <p><button class="btn btn-black adatta_testo1" type="submit" name="invia"><b>INVIA</b> </button></p>       
     </form>
     <?php
-    //include 'accessoDatabase.php';
 
     if(isset($_POST["invia"])){
-        //$con= accesso();
+
         $tipo= $_POST["opzione"];
-        //echo $tipo."<br>";
         $data= $_POST["data"];
-        //echo $data."<br>";
         $orauscita= $_POST["orauscita"];
-        //echo $orauscita;
         $cognomenomegenitore= $_POST["cognomenomegenitore"];
-        //echo $cognomenomegenitore."<br>";
         
         $idstudentesel= $_POST["idfiglio"];
         $query2= "SELECT * FROM login WHERE idUtente=$idstudentesel";
-        //echo $query;   
+   
         $result2=mysqli_query($con,$query2);
-        //echo mysqli_num_rows($result);
         if(mysqli_num_rows($result2)>0){ 
             $row= mysqli_fetch_array($result2);
             $cognomenomestudente= $row["cognome"]." ".$row["nome"];                          
             $classe= $row["classe"];             
         }
         
-        //$cognomenomestudente= $_POST["cognomenomestudente"];
-        //echo $cognomenomestudente."<br>";
-        //$classe= $_POST["classe"];
-        //echo $classe."<br>";
-        //$motivazione= $_POST["motivazione"];
-        //echo $motivazione."<br>";
         $note= $_POST["note"];
-        //echo $note."<br>";
         $fkUtente= $idstudentesel; //$_SESSION["idutente"];
-        //echo $fkUtente."<br>";
+
         //CONTROLLO DELLA DATA - NON DEVE ESSERE ANTECEDENTE L GIORNO CORRENTE
         $data_corrente= date("Y-m-d");
-        //echo $data_corrente;
+
         $data_corrente_esplosa= explode("-",$data_corrente); 
         $Y= $data_corrente_esplosa[0];
         $m= $data_corrente_esplosa[1];
         $d= $data_corrente_esplosa[2];
         $data_domani=  date('Y-m-d', mktime(0,0,0,date($m),date($d)+1,date($Y)));
-        //echo "OGGI: ". $data." DOMANI: ".$data_domani;
 
         if($data>=$data_corrente){ 
             //CONTROLLO CHE LA DATA NON SIA SUCCESSIVA A DOMANI
             if($__settings->config->limitaDataPermessi == true && $data>$data_domani){
-                //echo "<div style=color:red>Non è possibile creare PERMESSI nei giorni successivi a domani.</div>";
-                //echo "<div style=color:red>Selezionare una DATA corretta.</div>";  
+
                 $risposta= "Non è possibile creare PERMESSI nei giorni successivi a domani. Selezionare una DATA corretta";
                     echo "<script>alert('".$risposta."')</script>";
                     echo "<div class='alert alert-danger'>
@@ -202,17 +173,43 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif}
                         </div>";   
             }  
             else{
-                $risposta= "Il permesso è stato creato correttamente.";
-                echo "<script>alert('".$risposta."'); </script>";
-                echo "<div id='bottom' class='alert alert-success'>
-                            <strong>".$risposta."</strong> 
-                            </div>";
-                salvaPermesso($con,$tipo,$data,$orauscita,$cognomenomegenitore,$cognomenomestudente,$classe,$note,$fkUtente);
+
+                if( $browser->getBrowser() != Browser::BROWSER_IE && $browser->getBrowser() != Browser::BROWSER_SAFARI){
+                    //09:00:00 - 15:00:00
+                    if ($__settings->config->limitaOrarioPermessi == true)
+                    {
+                        if($ora<$_SESSION["permessi_ora_fine"]){
+                            $risposta= "Il permesso è stato creato correttamente.";
+                            echo "<script>alert('".$risposta."'); </script>";
+                            echo "<div id='bottom' class='alert alert-success'>
+                                        <strong>".$risposta."</strong> 
+                                        </div>";
+                            salvaPermesso($con,$tipo,$data,$orauscita,$cognomenomegenitore,$cognomenomestudente,$classe,$note,$fkUtente);
+                        }
+                        else{
+                            $risposta= "Non è più possibile creare PERMESSI per oggi. Selezionare una DATA corretta";
+                            echo "<script>alert('".$risposta."')</script>";
+                            echo "<div class='alert alert-danger'>
+                                <strong>".$risposta."</strong> 
+                                </div>";   
+                        }
+                    }
+                    else
+                    {
+                        $risposta= "Il permesso è stato creato correttamente.";
+                        echo "<script>alert('".$risposta."'); </script>";
+                        echo "<div id='bottom' class='alert alert-success'>
+                                    <strong>".$risposta."</strong> 
+                                    </div>";
+                        salvaPermesso($con,$tipo,$data,$orauscita,$cognomenomegenitore,$cognomenomestudente,$classe,$note,$fkUtente);
+                    }
+                }
+                else{
+                    echo "Il browser che si sta utilizzando non è aggiornato, si consiglia di usare Google Chrome per creare un permesso.";
+                }
             }
         }
         else{
-            //echo "<div style=color:red>Non è possibile creare PERMESSI nei giorni antecedenti al giorno corrente.</div>";
-            //echo "<div style=color:red>Selezionare una DATA corretta.</div>";
             $risposta= "Non è possibile creare PERMESSI nei giorni antecedenti al giorno corrente. Selezionare una DATA corretta";
             echo "<script>alert('".$risposta."'); </script>";
             echo "<div class='alert alert-danger'>
